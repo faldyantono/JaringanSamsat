5773281933e38d04efbf5b0305499b88bbeafc40
Faldy Antono <faldyant@gmail.com>
2022-03-15 18:19:39 +0700

    membuat struct method

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 func main() {
 	//output dari function parameter
 	modulego.SayHelloCensored("Haris", modulego.FilterSensor)
+	//struct method
+	var s1 kelengkapan
+	s1.KTP = 31748530911
+	s1.SIM = "Berlaku Hingga 2024"
+	s1.STNK = "Habis"
+	s1.administrasi()
 	//anonymous struct
 	vehicle := struct {
 		brand string
@@ -33,4 +39,15 @@ func main() {
 	//struct method
 	denda := modulego.Denda{2, 3, 0}
 	fmt.Println("Anda dikenakan Denda Sebesar				:", denda.Perkalian2(), "Rupiah")
+}
+
+type kelengkapan struct {
+	SIM, STNK string
+	KTP       int
+}
+
+func (identitas kelengkapan) administrasi() {
+	fmt.Println("Nomor Induk KTP                      			:", identitas.KTP)
+	fmt.Println("Masa Berlaku SIM                     			:", identitas.SIM)
+	fmt.Println("Masa Berlaku STNK                    			:", identitas.STNK)
 }
